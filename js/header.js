4e71bcb1f106cb3b09443e953f49925530e0fc18
Dov DEVERS <dov.devers@gmail.com>
2021-08-26 16:14:33 +0200

    update navbar + add skills section

--- js/header.js
+++ js/header.js
@@ -3,16 +3,17 @@ const header = document.getElementById("header");
 
 // Get the offset position of the navbar
 const sticky = header.offsetTop;
+var toggler = $("#navbar-toggler");
 
-window.addEventListener("scroll",headerStyle)
+window.addEventListener("scroll", headerStyle)
+window.addEventListener("resize", headerStyle)
 
 function headerStyle() {
-    if (window.pageYOffset > (sticky)) {
-        header.classList.add("sticky");
+    if (window.pageYOffset > (sticky)) header.classList.add("sticky");
 
-    } else {
-        header.classList.remove("sticky");
-    }
+    else if (toggler.css("display") === "block") header.classList.add("sticky");
+
+    else header.classList.remove("sticky");
 }
 
 headerStyle();
